cb920cabd7ee6208520af04bee71b4fa5e2dfbae
giancarissimo <giancarissimo@gmail.com>
2023-07-30 19:19:55 -0300

    agregue readme, index y el algoritmo de js

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let usuarioGuardado = "giancarissimo"
+let passwordGuardada = "1234"
+
+const login = () =>{
+    let ingresar = false
+    for(let i=3; i > 0; i--){
+        let usuarioIngresado = prompt(`Ingrese su usuario, tiene ${i} intentos`).toLowerCase().trim()
+        let passwordIngresada = prompt(`Ingrese su contraseña, tiene ${i} intentos`).trim()
+
+        if(usuarioGuardado === usuarioIngresado && passwordGuardada === passwordIngresada){
+            alert(`Bienvenido ${usuarioGuardado}!`)
+            ingresar = true
+            break
+        }else if(usuarioGuardado !== usuarioIngresado || passwordGuardada !== passwordIngresada){
+            alert(`Nombre de usuario y/o contraseña incorrectos.`)
+        }
+    }
+    return ingresar
+}
+
+if(login()){
+    const plazoDeDias = 30
+
+    function calcularPlazoFijo(montoInicial, tasaDeInteres, plazoDeDias) {
+        const tasaMensual = ((tasaDeInteres / 365) * plazoDeDias * montoInicial) / 100
+        const montoFinal = tasaMensual + montoInicial
+        return montoFinal
+    }
+
+    function calcularPlazoFijoA12Meses(){
+        let montoInicial = +(prompt("Ingrese el monto inicial (capital):")).trim()
+        let tasaDeInteres = +(prompt("Ingrese la tasa de interés anual (%):")).trim()
+
+        if (isNaN(montoInicial) || isNaN(tasaDeInteres)){
+            alert("Error: Ingrese datos válidos. Solo puede ingresar números.")
+            calcularPlazoFijoA12Meses()
+            return
+        }
+        const meses = 12
+        let montoActual = montoInicial
+
+        console.log(`Monto inicial: $${montoInicial}`)
+
+        for (let i = 1; i <= meses; i++) {
+            const montoFinalMes = calcularPlazoFijo(montoActual, tasaDeInteres, plazoDeDias)
+            console.log(`Monto después de ${i} meses: $${montoFinalMes.toFixed(2)}`)
+            montoActual = montoFinalMes
+        }
+    }
+    calcularPlazoFijoA12Meses()
+}else{
+    console.log(`Error: datos no válidos. Reinicie la página y vuelva a intentarlo.`)
+}
